d56630bef8692bb98e0c1f2c5e1c1d11a7d4e3a7
Gaurav Chavan <gaurav@appcd.com>
2026-01-20 16:00:06 +0000

    [skip ci] This PR is created by StackGen to validate connection.

--- iac-source1/terraform/modules/data_aws_availability_zones/data_aws_availability_zones.tf
+++
@@ -1,7 +0,0 @@
-data "aws_availability_zones" "this" {
-
-  all_availability_zones = var.all_availability_zones
-  exclude_names          = var.exclude_names
-  exclude_zone_ids       = var.exclude_zone_ids
-  state                  = var.state
-}
